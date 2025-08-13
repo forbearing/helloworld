af5e39469417bf1ebbf26ab1d6695d5cd7b2ecd3
cassius <condryhebenstreit@gmail.com>
2025-08-13 17:38:30 +0800

    update

--- service/twofa/setup/list.go
+++ service/twofa/setup/list.go
@@ -2,13 +2,13 @@ package setup
 
 import (
 	"errors"
+	"net/http"
+
 	"helloworld/model/twofa"
 	svc_twofa "helloworld/service/twofa"
-	"net/http"
 
 	"github.com/forbearing/golib/service"
 	"github.com/forbearing/golib/types"
-	"github.com/gin-gonic/gin"
 	"github.com/pquerna/otp/totp"
 	"github.com/skip2/go-qrcode"
 )
@@ -18,15 +18,13 @@ type Lister struct {
 }
 
 func (s *Lister) List(ctx *types.ServiceContext, req *twofa.Setup) (rsp *twofa.SetupRsp, err error) {
-	if err = generate2FA(ctx.GinContext); err != nil {
+	if err = generate2FA(ctx); err != nil {
 		return
 	}
 	return
 }
 
-func (s *Lister) ListBefore(ctx *types.ServiceContext, setups *[]* // UserSecrets[username] = key.Secret()
-twofa.Setup,
-) error {
+func (s *Lister) ListBefore(ctx *types.ServiceContext, setups *[]*twofa.Setup) error {
 	log := s.WithServiceContext(ctx, ctx.GetPhase())
 	log.Info("setup list before")
 	return nil
@@ -38,7 +36,8 @@ func (s *Lister) ListAfter(ctx *types.ServiceContext, setups *[]*twofa.Setup) er
 	return nil
 }
 
-func generate2FA(c *gin.Context) error {
+func generate2FA(ctx *types.ServiceContext) error {
+	c := ctx.GinContext
 	username := c.Query("user")
 	if username == "" {
 		return errors.New("missing user")
